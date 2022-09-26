c573b857546cda2c9021d39f265c647072f84aba
Hafizur Rahman <coderpickbd@gmail.com>
2022-09-26 15:51:44 +0600

    auth api create done

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        /* validate request */
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            $response = [
+                'success' => false,
+                'message' => $validator->errors()
+            ];
+            return response()->json($response, 400);
+        }
+
+        $input = $request->all();
+
+        $input['password'] = bcrypt($input['password']);
+
+        $user = User::create($input);
+
+        $data['token'] = $user->createToken('MyApp')->plainTextToken;
+        $data['name'] = $user->name;
+
+        $response = [
+            'success' => true,
+            'message' => "User register successfully",
+            'data' => $data,
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    public function login(Request $request)
+    {
+
+        /* validate request */
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            $response = [
+                'success' => false,
+                'message' => $validator->errors()
+            ];
+            return response()->json($response, 400);
+        }
+
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            $data['token'] = $user->createToken('MyApp')->plainTextToken;
+            $data['name'] = $user->name;
+
+            $response = [
+                'success' => true,
+                'message' => "User login successfully",
+                'data' => $data,
+            ];
+
+            return response()->json($response, 200);
+        } else {
+
+
+            $response = [
+                'success' => fake(),
+                'message' => "Unauthorized",
+
+            ];
+
+            return response()->json($response, 401);
+        }
+    }
+}
